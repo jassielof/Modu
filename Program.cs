23ddc550ffc5a71005aec563b88541715717217a
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-07-24 23:45:09 -0400

    newline

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-void Print(string message)
-{
-    Console.WriteLine();
-    string hola = "";
-}
